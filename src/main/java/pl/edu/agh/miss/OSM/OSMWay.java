cc83c9aaf4483e37a9c669675974812690240f67
Krzysztof Slonka <slonka@student.agh.edu.pl>
2015-04-27 20:14:29 +0200

    Add junit and test - not working though

--- src/main/java/pl/edu/agh/miss/OSM/OSMWay.java
+++ src/main/java/pl/edu/agh/miss/OSM/OSMWay.java
@@ -2,11 +2,13 @@ package pl.edu.agh.miss.OSM;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by slonka on 27.04.15.
  */
 public class OSMWay {
-    ArrayList<OSMNode> nodes;
-    HashMap<String,String> tags;
+    List nodes = new ArrayList<OSMNode>();
+    Map tags = new HashMap<String, String>();
 }
