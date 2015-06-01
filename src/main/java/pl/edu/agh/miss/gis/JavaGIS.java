d2d7f18dacd32f1b827895c2454b71b321b55679
slonka <krzysztofslonka@gmail.com>
2015-06-01 19:37:28 +0200

    Refactor code

--- src/main/java/pl/edu/agh/miss/gis/JavaGIS.java
+++ src/main/java/pl/edu/agh/miss/gis/JavaGIS.java
@@ -1,47 +1,53 @@
 package pl.edu.agh.miss.gis;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class JavaGIS {
 
     public static void main(String[] args) {
 
-        java.sql.Connection conn;
+        Connection conn;
 
         try {
-            //Load the JDBC driver and establish a connection.
-
-            Class.forName("org.postgresql.Driver");
-            String pgsql_db = System.getenv("PGSQL_DB");
-            String pgsql_user = System.getenv("PGSQL_USER");
-            String url = "jdbc:postgresql://localhost:5432/" + pgsql_db;
-            String pgsql_password = System.getenv("PGSQL_PASSWORD");
-            System.out.println(url + " " + pgsql_user + " " + pgsql_password);
-            conn = DriverManager.getConnection(url, pgsql_user, pgsql_password);
-
-            // Create a statement and execute a select query.
-
+            conn = getConnection();
             Statement s = conn.createStatement();
-            String newline = System.getProperty("line.separator");
-            String query = "SELECT seq, id1 AS node, id2 AS edge, cost FROM pgr_dijkstra('" + newline +
-                    "SELECT gid AS id," + newline +
-                    "source::integer," + newline +
-                    "target::integer," + newline +
-                    "length::double precision AS cost" + newline +
-                    "FROM ways'," + newline +
-                    "10, 9, false, false)";
-
+            String query = buildRouteQuery("pgr_dijkstra", 10, 9);
             ResultSet r = s.executeQuery(query);
-            while (r.next()) {
-                float cost = r.getFloat("cost");
-                System.out.println("Cost at " + cost);
-            }
+            processQuery(r);
             s.close();
             conn.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void processQuery(ResultSet r) throws SQLException {
+        while (r.next()) {
+            float cost = r.getFloat("cost");
+            System.out.println("Cost at " + cost);
+        }
+    }
+
+    private static String buildRouteQuery(String algorithm, int nodeIdStart, int nodeIdEnd) {
+        String newline = System.getProperty("line.separator");
+        return "SELECT seq, id1 AS node, id2 AS edge, cost FROM " + algorithm + "('" + newline +
+                "SELECT gid AS id," + newline +
+                "source::integer," + newline +
+                "target::integer," + newline +
+                "length::double precision AS cost" + newline +
+                "FROM ways'," + newline +
+                nodeIdStart+", "+nodeIdEnd+", false, false)";
+    }
+
+    private static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Connection conn;
+        Class.forName("org.postgresql.Driver");
+        String pgsql_db = System.getenv("PGSQL_DB");
+        String pgsql_user = System.getenv("PGSQL_USER");
+        String url = "jdbc:postgresql://localhost:5432/" + pgsql_db;
+        String pgsql_password = System.getenv("PGSQL_PASSWORD");
+        System.out.println(url + " " + pgsql_user + " " + pgsql_password);
+        conn = DriverManager.getConnection(url, pgsql_user, pgsql_password);
+        return conn;
     }
 }
---
+++ src/main/java/pl/edu/agh/miss/solver/gis/PostgisSolver.java
@@ -0,0 +1,17 @@
+package pl.edu.agh.miss.solver.gis;
+
+import pl.edu.agh.miss.map.Map;
+import pl.edu.agh.miss.map.Node;
+import pl.edu.agh.miss.path.Path;
+import pl.edu.agh.miss.solver.ISolver;
+
+/**
+ * Created by slonka on 01.06.15.
+ */
+public class PostgisSolver implements ISolver{
+
+    @Override
+    public Path findPath(Node start, Node end, Map map) {
+        return null;
+    }
+}
